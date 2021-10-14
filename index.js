8e8b4b5d66f77b65236be45acea20a97b3ee14fe
Rajeswar Sharma <54684919+RajeswarSharma@users.noreply.github.com>
2021-10-14 19:36:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,9 @@ app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_SESSION_KEY],
-    //httpOnly:true,
+    httpOnly:true,
+    secure:true,
+    sameSite:"none"
   })
 );
 app.use(cookieParser());
