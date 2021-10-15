33e214fcebbaff1ef487f737763a9efc3e4a99e5
Restyled.io <commits@restyled.io>
2021-10-15 21:45:25 +0000

    Restyled by whitespace

--- templates/contact_ack.js
+++ templates/contact_ack.js
@@ -10,8 +10,8 @@ exports.getTemplate = (userEmail, subject) => {
     <title></title>
     <!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge" /><!--<![endif]-->
     <meta name="viewport" content="width=device-width" /><style type="text/css">
-@media only screen and (min-width: 620px){.wrapper{min-width:600px !important}.wrapper h1{}.wrapper h1{font-size:36px !important;line-height:43px !important}.wrapper h2{}.wrapper h2{font-size:22px !important;line-height:31px !important}.wrapper h3{}.wrapper h3{font-size:18px !important;line-height:26px !important}.column{}.wrapper .size-8{font-size:8px !important;line-height:14px !important}.wrapper .size-9{font-size:9px !important;line-height:16px !important}.wrapper .size-10{font-size:10px !important;line-height:18px !important}.wrapper .size-11{font-size:11px !important;line-height:19px !important}.wrapper .size-12{font-size:12px !important;line-height:19px !important}.wrapper .size-13{font-size:13px !important;line-height:21px !important}.wrapper .size-14{font-size:14px !important;line-height:21px !important}.wrapper .size-15{font-size:15px !important;line-height:23px 
-!important}.wrapper .size-16{font-size:16px !important;line-height:24px !important}.wrapper .size-17{font-size:17px !important;line-height:26px !important}.wrapper .size-18{font-size:18px !important;line-height:26px !important}.wrapper .size-20{font-size:20px !important;line-height:28px !important}.wrapper .size-22{font-size:22px !important;line-height:31px !important}.wrapper .size-24{font-size:24px !important;line-height:32px !important}.wrapper .size-26{font-size:26px !important;line-height:34px !important}.wrapper .size-28{font-size:28px !important;line-height:36px !important}.wrapper .size-30{font-size:30px !important;line-height:38px !important}.wrapper .size-32{font-size:32px !important;line-height:40px !important}.wrapper .size-34{font-size:34px !important;line-height:43px !important}.wrapper .size-36{font-size:36px !important;line-height:43px !important}.wrapper 
+@media only screen and (min-width: 620px){.wrapper{min-width:600px !important}.wrapper h1{}.wrapper h1{font-size:36px !important;line-height:43px !important}.wrapper h2{}.wrapper h2{font-size:22px !important;line-height:31px !important}.wrapper h3{}.wrapper h3{font-size:18px !important;line-height:26px !important}.column{}.wrapper .size-8{font-size:8px !important;line-height:14px !important}.wrapper .size-9{font-size:9px !important;line-height:16px !important}.wrapper .size-10{font-size:10px !important;line-height:18px !important}.wrapper .size-11{font-size:11px !important;line-height:19px !important}.wrapper .size-12{font-size:12px !important;line-height:19px !important}.wrapper .size-13{font-size:13px !important;line-height:21px !important}.wrapper .size-14{font-size:14px !important;line-height:21px !important}.wrapper .size-15{font-size:15px !important;line-height:23px
+!important}.wrapper .size-16{font-size:16px !important;line-height:24px !important}.wrapper .size-17{font-size:17px !important;line-height:26px !important}.wrapper .size-18{font-size:18px !important;line-height:26px !important}.wrapper .size-20{font-size:20px !important;line-height:28px !important}.wrapper .size-22{font-size:22px !important;line-height:31px !important}.wrapper .size-24{font-size:24px !important;line-height:32px !important}.wrapper .size-26{font-size:26px !important;line-height:34px !important}.wrapper .size-28{font-size:28px !important;line-height:36px !important}.wrapper .size-30{font-size:30px !important;line-height:38px !important}.wrapper .size-32{font-size:32px !important;line-height:40px !important}.wrapper .size-34{font-size:34px !important;line-height:43px !important}.wrapper .size-36{font-size:36px !important;line-height:43px !important}.wrapper
 .size-40{font-size:40px !important;line-height:47px !important}.wrapper .size-44{font-size:44px !important;line-height:50px !important}.wrapper .size-48{font-size:48px !important;line-height:54px !important}.wrapper .size-56{font-size:56px !important;line-height:60px !important}.wrapper .size-64{font-size:64px !important;line-height:63px !important}}
 </style>
     <meta name="x-apple-disable-message-reformatting" />
@@ -278,7 +278,7 @@ div.preheader {
   .email-flexible-footer__additionalinfo--center {
     text-align: center !important; /* [1] */
   }
-  
+
   .email-flexible-footer .left-aligned-footer table,
   .email-flexible-footer .center-aligned-footer table,
   .email-flexible-footer .right-aligned-footer table {
@@ -478,7 +478,7 @@ div.preheader {
   line-height: 63px !important;
 }
 </style>
-    
+
   <!--[if !mso]><!--><style type="text/css">
 @import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);
 </style><link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic" rel="stylesheet" type="text/css" /><!--<![endif]--><style type="text/css">
@@ -498,58 +498,58 @@ div.preheader {
           <div style="border-collapse: collapse;display: table;width: 100%;">
           <!--[if (mso)|(IE)]><table align="center" class="preheader" cellpadding="0" cellspacing="0" role="presentation"><tr><td style="width: 280px" valign="top"><![endif]-->
             <div class="snippet" style="display: table-cell;Float: left;font-size: 12px;line-height: 19px;max-width: 280px;min-width: 140px; width: 140px;width: calc(14000% - 78120px);padding: 10px 0 5px 0;color: #787778;font-family: Ubuntu,sans-serif;">
-              
+
             </div>
           <!--[if (mso)|(IE)]></td><td style="width: 280px" valign="top"><![endif]-->
             <div class="webversion" style="display: table-cell;Float: left;font-size: 12px;line-height: 19px;max-width: 280px;min-width: 139px; width: 139px;width: calc(14100% - 78680px);padding: 10px 0 5px 0;text-align: right;color: #787778;font-family: Ubuntu,sans-serif;">
-              
+
             </div>
           <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
           </div>
         </div>
-        
+
       </div>
       <div>
       <div class="layout one-col fixed-width stack" style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;">
         <div class="layout__inner" style="border-collapse: collapse;display: table;width: 100%;background-color: #ffffff;">
         <!--[if (mso)|(IE)]><table align="center" cellpadding="0" cellspacing="0" role="presentation"><tr class="layout-fixed-width" style="background-color: #ffffff;"><td style="width: 600px" class="w560"><![endif]-->
           <div class="column" style="text-align: left;color: #787778;font-size: 16px;line-height: 24px;font-family: Ubuntu,sans-serif;">
-        
+
             <div style="Margin-left: 20px;Margin-right: 20px;Margin-top: 24px;">
       <div style="mso-line-height-rule: exactly;line-height: 20px;font-size: 1px;">&nbsp;</div>
     </div>
-        
+
             <div style="Margin-left: 20px;Margin-right: 20px;">
       <div style="mso-line-height-rule: exactly;mso-text-raise: 11px;vertical-align: middle;">
         <h1 style="Margin-top: 0;Margin-bottom: 0;font-style: normal;font-weight: normal;color: #565656;font-size: 30px;line-height: 38px;text-align: center;"><strong>We value your feedback!</strong></h1><p style="Margin-top: 20px;Margin-bottom: 0;">&nbsp;<br />
 Hi there, thank you very much for writing to us. We have recived your message on the subject <b>${subject}</b>.<br/> We will get back to you in 2-3 working days.</p><p style="Margin-top: 20px;Margin-bottom: 20px;">The information you provide will help us to improve our customer experience for you and the entire cucoders community.<br/><br>cucoders</p>
       </div>
     </div>
-        
+
             <div style="Margin-left: 20px;Margin-right: 20px;">
       <div style="mso-line-height-rule: exactly;line-height: 10px;font-size: 1px;">&nbsp;</div>
     </div>
             <div style="Margin-left: 20px;Margin-right: 20px;">
       <div style="mso-line-height-rule: exactly;line-height: 10px;font-size: 1px;">&nbsp;</div>
     </div>
-        
+
             <div style="Margin-left: 20px;Margin-right: 20px;">
       <div style="mso-line-height-rule: exactly;mso-text-raise: 11px;vertical-align: middle;">
         <p style="Margin-top: 0;Margin-bottom: 20px;"><em>All of your messages are confidential and will not be shared outside of cucoders.</em></p>
       </div>
     </div>
-        
+
             <div style="Margin-left: 20px;Margin-right: 20px;Margin-bottom: 24px;">
       <div style="mso-line-height-rule: exactly;line-height: 5px;font-size: 1px;">&nbsp;</div>
     </div>
-        
+
           </div>
         <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
         </div>
       </div>
-  
+
       <div style="mso-line-height-rule: exactly;line-height: 10px;font-size: 10px;">&nbsp;</div>
-  
+
       </div>
       <div role="contentinfo"><div style="line-height:4px;font-size:4px;" id="footer-top-spacing">&nbsp;</div><div class="layout email-flexible-footer email-footer" style="Margin: 0 auto;max-width: 600px;min-width: 320px; width: 320px;width: calc(28000% - 167400px);overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;" dir="rtl" id="footer-content">
       <div class="layout__inner right-aligned-footer" style="border-collapse: collapse;display: table;width: 100%;">
@@ -562,7 +562,7 @@ Hi there, thank you very much for writing to us. We have recived your message on
       </div>
     </div>
         <!--[if (mso)|(IE)]></td><![endif]-->
-        <!--[if (mso)|(IE)]><td valign="top" class="w60"><![endif]-->      
+        <!--[if (mso)|(IE)]><td valign="top" class="w60"><![endif]-->
 </body></html>
 `,
   };
