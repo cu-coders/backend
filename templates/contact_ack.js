3b2bf01f1b4b16c32bf34db83120aa6175a2c2e3
Ankit Raj <ankit.raj928@gmail.com>
2021-10-17 00:04:03 +0530

    Update contact_ack.js

--- templates/contact_ack.js
+++ templates/contact_ack.js
@@ -2,7 +2,7 @@ exports.getTemplate = (userEmail, subject) => {
   const message = {
     from: process.env.EMAIL_SENDER,
     to: userEmail,
-    subject: "Cucoders-Acknowledgement mail",
+    subject: "CU-Chapter-Acknowledgement mail",
     text: `
     Thank you for writing to us. We have received your message about ${subject} and will get back to you within 2-3 working days.`,
     html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -290,7 +290,7 @@ exports.getTemplate = (userEmail, subject) => {
 											<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 25px; padding-top: 10px; padding-bottom: 10px; font-family: Tahoma, sans-serif"><![endif]-->
 											<div style="color:#61697a;font-family:Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:25px;">
 												<div class="txtTinyMce-wrapper" style="line-height: 1.2; font-size: 12px; color: #61697a; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 14px;">
-													<p style="margin: 0; font-size: 14px; line-height: 1.2; word-break: break-word; mso-line-height-alt: 17px; margin-top: 0; margin-bottom: 0;">Hi {firstname},</p>
+													<p style="margin: 0; font-size: 14px; line-height: 1.2; word-break: break-word; mso-line-height-alt: 17px; margin-top: 0; margin-bottom: 0;">Hi {username},</p>
 												</div>
 											</div>
 											<!--[if mso]></td></tr></table><![endif]-->
