1297ea3e836cc121f55afc794ad4b7ef9512cb2a
RajeswarSharma <rjqwer47@gmail.com>
2021-10-17 12:22:21 +0530

    updated header options

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const passport = require("passport");
 //-------------------------------------------DATABASE CONNECTION
 //SETUP----------------------------------------//
 const app = express();
-app.set("x-powered-by","cu-coders");
+app.set("X-Powered-By", "cu-coders");
 const PORT = process.env.PORT || 3001;
 
 // Demo database: Connect to a actual database before deployment
