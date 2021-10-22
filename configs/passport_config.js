6f20943782a53717f016bc8cc83ddbd1ca18e977
Restyled.io <commits@restyled.io>
2021-10-22 14:58:44 +0000

    Restyled by clang-format

--- configs/passport_config.js
+++ configs/passport_config.js
@@ -6,108 +6,97 @@ const bcrypt = require("bcrypt");
 const User = require("../models/users");
 
 //-------------------------------------------------END OF
-//IMPORTS---------------------------------------------//
+// IMPORTS---------------------------------------------//
 
 //----------------------------------------------GOOGLE OAUTH
-//STRATEGY-----------------------------------------//
-passport.use(
-  new GoogleStrategy(
+// STRATEGY-----------------------------------------//
+passport.use(new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://main-cu-coders.herokuapp.com/auth/google/redirect",
+      clientID : process.env.GOOGLE_CLIENT_ID,
+      clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL : "https://main-cu-coders.herokuapp.com/auth/google/redirect",
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ email: profile.emails[0].value }).then((oldUser) => {
+      User.findOne({email : profile.emails[0].value}).then((oldUser) => {
         if (oldUser) {
           done(null, oldUser._id);
         } else {
           new User({
-            firstname: profile.name.givenName,
-            lastname: profile.name.familyName,
-            email: profile.emails[0].value,
-            third_partyID: profile.id,
-            auth_type: "google",
-            password: null,
-            mailtoken: null,
-            isactive: true,
+            firstname : profile.name.givenName,
+            lastname : profile.name.familyName,
+            email : profile.emails[0].value,
+            third_partyID : profile.id,
+            auth_type : "google",
+            password : null,
+            mailtoken : null,
+            isactive : true,
           })
-            .save()
-            .then((newUser) => {
-              done(null, newUser._id);
-            });
+              .save()
+              .then((newUser) => { done(null, newUser._id); });
         }
       });
-    }
-  )
-);
+    }));
 //-------------------------------------END OF GOOGLE OAUTH
-//STRATEGY-----------------------------------//
+// STRATEGY-----------------------------------//
 
 //----------------------------------------- GITHUB
-//STRATEGY------------------------------------------//
+// STRATEGY------------------------------------------//
 
-passport.use(
-  new GitHubStrategy(
+passport.use(new GitHubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "https://main-cu-coders.herokuapp.com/auth/github/redirect",
-      scope: ["user:email"],
+      clientID : process.env.GITHUB_CLIENT_ID,
+      clientSecret : process.env.GITHUB_CLIENT_SECRET,
+      callbackURL : "https://main-cu-coders.herokuapp.com/auth/github/redirect",
+      scope : [ "user:email" ],
     },
     (accessToken, refreshToken, profile, done) => {
-      //console.log(profile);
-      User.findOne({ email: profile.emails[0].value }).then((oldUser) => {
+      // console.log(profile);
+      User.findOne({email : profile.emails[0].value}).then((oldUser) => {
         if (oldUser) {
           // User with the same email already exists
           done(null, oldUser._id);
         } else {
           // New User
           new User({
-            firstname: profile.displayName,
-            lastname: null,
-            email: profile.emails[0].value,
-            third_partyID: profile.id,
-            auth_type: "github",
-            password: null,
-            mailtoken: null,
-            isactive: true,
+            firstname : profile.displayName,
+            lastname : null,
+            email : profile.emails[0].value,
+            third_partyID : profile.id,
+            auth_type : "github",
+            password : null,
+            mailtoken : null,
+            isactive : true,
           })
-            .save()
-            .then((newUser) => {
-              done(null, newUser._id);
-            });
+              .save()
+              .then((newUser) => { done(null, newUser._id); });
         }
       });
-    }
-  )
-);
+    }));
 
 //--------------------------------------END OF GITHUB
-//STRATEGY----------------------------------------//
+// STRATEGY----------------------------------------//
 
 //-------------------------------------------LOCAL
-//STRATEGY-------------------------------------------//
-passport.use(
-  new LocalStrategy(
-    { usernameField: "email", passwordField: "password", session: true },
+// STRATEGY-------------------------------------------//
+passport.use(new LocalStrategy(
+    {usernameField : "email", passwordField : "password", session : true},
     (email, password, done) => {
-      User.findOne({ email: email }, (err, user) => {
+      User.findOne({email : email}, (err, user) => {
         if (err) {
           return done(err);
         }
         // If the user is invalid
         if (!user) {
           return done(null, false, {
-            success: false,
-            message: "unregistered email",
+            success : false,
+            message : "unregistered email",
           });
         }
         // Password is null i.e registered using google or github
         if (!user.password) {
           return done(null, false, {
-            message: "Invalid login mode",
-            success: false,
+            message : "Invalid login mode",
+            success : false,
           });
         }
         bcrypt.compare(password, user.password).then((isValid) => {
@@ -117,26 +106,22 @@ passport.use(
               return done(null, user._id);
             }
             return done(null, false, {
-              message: "Please verify your email first",
+              message : "Please verify your email first",
             });
           }
           // incorrect password
           return done(null, false, {
-            message: "Invalid Credentials",
-            success: false,
+            message : "Invalid Credentials",
+            success : false,
           });
         });
       });
-    }
-  )
-);
+    }));
 //--------------------------------------------------END  OF LOCAL
-//STRATEGY----------------------------------------//
+// STRATEGY----------------------------------------//
 //------------------------------------------------SERIALIZERS AND
-//DESERIALIZERS-----------------------------------//
-passport.serializeUser((id, done) => {
-  done(null, id);
-});
+// DESERIALIZERS-----------------------------------//
+passport.serializeUser((id, done) => { done(null, id); });
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then((user) => {
@@ -146,4 +131,4 @@ passport.deserializeUser((id, done) => {
   });
 });
 //------------------------------------------END OF SERIALIZERS AND
-//DESERIALIZERS----------------------------------//
+// DESERIALIZERS----------------------------------//
--- middlewares/validation-rules.js
+++ middlewares/validation-rules.js
@@ -1,34 +1,32 @@
-const { check } = require("express-validator");
+const {check} = require("express-validator");
 //--------------------------------END OF
-//IMPORTS---------------------------------------//
+// IMPORTS---------------------------------------//
 
 // validation rules for contact forms
 exports.contactForms = [
   check("fullname")
-    .trim()
-    .notEmpty()
-    .withMessage("Fullname is required")
-    .matches(/^[a-zA-Z*$]/)
-    .withMessage("Only characters with white space are allowed"),
+      .trim()
+      .notEmpty()
+      .withMessage("Fullname is required")
+      .matches(/^[a-zA-Z*$]/)
+      .withMessage("Only characters with white space are allowed"),
   check("email")
-    .trim()
-    .notEmpty()
-    .withMessage("Email address is required")
-    .normalizeEmail()
-    .isEmail()
-    .withMessage("Invalid Email address"),
+      .trim()
+      .notEmpty()
+      .withMessage("Email address is required")
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Invalid Email address"),
   check("subject").trim().notEmpty().withMessage("Subject is empty"),
   check("message").trim().notEmpty().withMessage("Message is be empty"),
 ];
 
 exports.resetForm = [
   check("email")
-    .trim()
-    .notEmpty()
-    .withMessage("Email address is required")
-    .isEmail()
-    .withMessage("Invalid Email address"),
-  check("password")
-    .notEmpty()
-    .withMessage("Password cannot be empty")
+      .trim()
+      .notEmpty()
+      .withMessage("Email address is required")
+      .isEmail()
+      .withMessage("Invalid Email address"),
+  check("password").notEmpty().withMessage("Password cannot be empty")
 ];
