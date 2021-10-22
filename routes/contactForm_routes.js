1be62e86637f578f4192140f143588884ba0e433
RajeswarSharma <rjqwer47@gmail.com>
2021-10-22 18:56:20 +0530

    minor fixes

--- routes/contactForm_routes.js
+++ routes/contactForm_routes.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 const rules = require("../middlewares/validation-rules");
 const contactUsDbApis = require("../controllers/contactUsDbApis");
 const router = express.Router();
-router.post("/",rules.contactForms,async (req, res) => {
+router.post("/", rules.contactForms, async (req, res) => {
   const validationErr = validationResult(req);
   if (validationErr.isEmpty()) {
     contactUsDbApis.insertMessage(req, res);
