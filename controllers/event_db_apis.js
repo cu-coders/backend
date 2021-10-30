4fe8c421b89f1f50dadcdb966304bab058bfed8a
Restyled.io <commits@restyled.io>
2021-10-30 16:40:53 +0000

    Restyled by prettier

--- controllers/event_db_apis.js
+++ controllers/event_db_apis.js
@@ -9,16 +9,16 @@ exports.insert_event = async (req, res, imageURL, public_id) => {
   try {
     const tempData = req.body;
     const event = new Event({
-      imageSrc : imageURL,
-      imageId : public_id,
-      author : tempData.author,
-      category : tempData.category,
-      title : tempData.title,
-      subtitle : tempData.subtitle,
-      description : tempData.description,
-      url : tempData.url,
-      date_start : new Date(tempData.date_start).getTime(),
-      date_end : new Date(tempData.date_end).getTime(),
+      imageSrc: imageURL,
+      imageId: public_id,
+      author: tempData.author,
+      category: tempData.category,
+      title: tempData.title,
+      subtitle: tempData.subtitle,
+      description: tempData.description,
+      url: tempData.url,
+      date_start: new Date(tempData.date_start).getTime(),
+      date_end: new Date(tempData.date_end).getTime(),
     });
     await event.save();
   } catch (err) {
@@ -30,9 +30,9 @@ exports.insert_event = async (req, res, imageURL, public_id) => {
 exports.read_ongoing_events = async (req, res) => {
   try {
     const data = await Event.find({
-      $and : [
-        {date_start : {$lte : Date.now()}},
-        {date_end : {$gte : Date.now()}},
+      $and: [
+        { date_start: { $lte: Date.now() } },
+        { date_end: { $gte: Date.now() } },
       ],
     });
     return data;
@@ -44,7 +44,7 @@ exports.read_ongoing_events = async (req, res) => {
 // API for upcomming events
 exports.read_upcomming_events = async () => {
   try {
-    const data = await Event.find({date_start : {$gt : Date.now()}});
+    const data = await Event.find({ date_start: { $gt: Date.now() } });
     return data;
   } catch (err) {
     console.log(err);
@@ -54,7 +54,7 @@ exports.read_upcomming_events = async () => {
 // API for past events
 exports.read_past_events = async () => {
   try {
-    const data = await Event.find({date_end : {$lt : Date.now()}});
+    const data = await Event.find({ date_end: { $lt: Date.now() } });
     return data;
   } catch (err) {
     console.log(err);
