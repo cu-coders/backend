3489349aa346254e75a4d92acafabfe7b5643f11
RajeswarSharma <rjqwer47@gmail.com>
2021-10-30 22:28:37 +0530

    created db schema for team

---
+++ models/team.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const teamSchema = mongoose.Schema({
+  profileImage: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  fullname: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  linkedin: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  instagram: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  github: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+});
+const Team = mongoose.model("team", teamSchema);
+module.exports = Team;
