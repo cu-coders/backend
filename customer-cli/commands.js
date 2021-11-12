1a13c50e8e7561d409e95d5bc147a7b8282ac047
puranjay123 <kwattrapuranjay@gmail.com>
2021-11-12 19:12:36 +0530

    Added CLI for the  job applying(Needs Review)

---
+++ customer-cli/commands.js
@@ -0,0 +1,25 @@
+const program = require('commander');
+const{
+    addCustomer,
+    findCustomer
+} = require('./index');
+
+program
+    .version('1.0.0')
+    .description('CUcoders Managment CLI ')
+
+program
+    .command('add <firstname> <lastname> <phone> <email>')
+    .alias('a')
+    .description('Add your details to apply for Job')
+    .action((firstname,lastname,phone,email)=> {
+        addCustomer({firstname,lastname,phone,email});
+    });
+
+program
+    .command('find <name>')
+    .alias('f')
+    .description('Find an applicant')
+    .action(name => findCustomer(name));
+
+program.parse(process.argv);
