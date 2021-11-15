fe051aa80bb21c010d7afc6cfe6d85001aa1748d
RajeswarSharma <rjqwer47@gmail.com>
2021-11-15 09:43:00 +0530

    Created route for job application, Configured multer and cloudinary

--- routes/job_routes.js
+++ routes/job_routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const { uploadDoc } = require("../configs/multer_config");
 const { addJobApplication } = require("../controllers/jobDBApis");
-router.post("apply", uploadDoc.single("resume"), (req, res) => {
+router.post("/apply", uploadDoc.single("resume"), (req, res) => {
   try {
     addJobApplication(req, res);
   } catch (error) {
