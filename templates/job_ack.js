580f275e32a749805b79b3bf50a2de09d9c1bfea
RajeswarSharma <rjqwer47@gmail.com>
2021-11-19 15:11:36 +0530

    Updated the template

--- templates/job_ack.js
+++ templates/job_ack.js
@@ -340,7 +340,7 @@ exports.getTemplate = (userEmail, role, name) => {
 											<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 5px; font-family: Tahoma, sans-serif"><![endif]-->
 											<div style="color:#555555;font-family:Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:10px;padding-bottom:5px;padding-left:10px;">
 												<div class="txtTinyMce-wrapper" style="line-height: 1.2; font-size: 12px; color: #555555; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 14px;">
-													<p style="margin: 0; font-size: 18px; line-height: 1.2; text-align: center; word-break: break-word; mso-line-height-alt: 22px; margin-top: 0; margin-bottom: 0;"><span style="color: #2b303a; font-size: 18px;">This letter is to inform you that we have received your application for <strong>${role}</strong>. We appreciate your interest in <strong>CUCODERS</strong>.
+													<p style="margin: 0; font-size: 18px; line-height: 1.2; text-align: center; word-break: break-word; mso-line-height-alt: 22px; margin-top: 0; margin-bottom: 0;"><span style="color: #2b303a; font-size: 18px;">This letter is to inform you that we have received your application for the role <strong>${role}</strong>. We appreciate your interest in <strong>CUCODERS</strong>.
 <br>
 We are procesing your application. If you are selected for an interview, our human resources department will be in contact with you soon.</p>
 					</span></p>
