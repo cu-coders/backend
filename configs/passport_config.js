cdfd1831c883b5d7e1fe46fa94cdfff0f71cf42f
Restyled.io <commits@restyled.io>
2022-02-10 07:17:20 +0000

    Restyled by prettier

--- configs/passport_config.js
+++ configs/passport_config.js
@@ -12,15 +12,16 @@ const User = require("../models/users");
 
 //----------------------------------------------GOOGLE OAUTH
 // STRATEGY-----------------------------------------//
-passport.use(new GoogleStrategy(
+passport.use(
+  new GoogleStrategy(
     {
-      clientID : process.env.GOOGLE_CLIENT_ID,
-      clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL : "https://main-cu-coders.herokuapp.com/auth/google/redirect",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://main-cu-coders.herokuapp.com/auth/google/redirect",
     },
     (accessToken, refreshToken, profile, done) => {
       const email = sanitize(profile.emails[0].value);
-      User.findOne({email}).then((oldUser) => {
+      User.findOne({ email }).then((oldUser) => {
         if (oldUser) {
           done(null, oldUser._id);
         } else {
@@ -32,33 +33,38 @@ passport.use(new GoogleStrategy(
             lastname,
             email,
             third_partyID,
-            auth_type : "google",
-            password : null,
-            mailtoken : null,
-            isactive : true,
+            auth_type: "google",
+            password: null,
+            mailtoken: null,
+            isactive: true,
           })
-              .save()
-              .then((newUser) => { done(null, newUser._id); });
+            .save()
+            .then((newUser) => {
+              done(null, newUser._id);
+            });
         }
       });
-    }));
+    }
+  )
+);
 //-------------------------------------END OF GOOGLE OAUTH
 // STRATEGY-----------------------------------//
 
 //----------------------------------------- GITHUB
 // STRATEGY------------------------------------------//
 
-passport.use(new GitHubStrategy(
+passport.use(
+  new GitHubStrategy(
     {
-      clientID : process.env.GITHUB_CLIENT_ID,
-      clientSecret : process.env.GITHUB_CLIENT_SECRET,
-      callbackURL : "https://main-cu-coders.herokuapp.com/auth/github/redirect",
-      scope : [ "user:email" ],
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "https://main-cu-coders.herokuapp.com/auth/github/redirect",
+      scope: ["user:email"],
     },
     (accessToken, refreshToken, profile, done) => {
       // console.log(profile);
       const email = sanitize(profile.emails[0].value);
-      User.findOne({email}).then((oldUser) => {
+      User.findOne({ email }).then((oldUser) => {
         if (oldUser) {
           // User with the same email already exists
           done(null, oldUser._id);
@@ -67,45 +73,50 @@ passport.use(new GitHubStrategy(
           const third_partyID = sanitize(profile.id);
           new User({
             firstname,
-            lastname : null,
+            lastname: null,
             email,
             third_partyID,
-            auth_type : "github",
-            password : null,
-            mailtoken : null,
-            isactive : true,
+            auth_type: "github",
+            password: null,
+            mailtoken: null,
+            isactive: true,
           })
-              .save()
-              .then((newUser) => { done(null, newUser._id); });
+            .save()
+            .then((newUser) => {
+              done(null, newUser._id);
+            });
         }
       });
-    }));
+    }
+  )
+);
 
 //--------------------------------------END OF GITHUB
 // STRATEGY----------------------------------------//
 
 //-------------------------------------------LOCAL
 // STRATEGY-------------------------------------------//
-passport.use(new LocalStrategy(
-    {usernameField : "email", passwordField : "password", session : true},
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password", session: true },
     (email, password, done) => {
       const sEmail = sanitize(email);
-      User.findOne({email : sEmail}, (err, user) => {
+      User.findOne({ email: sEmail }, (err, user) => {
         if (err) {
           return done(err);
         }
         // If the user is invalid
         if (!user) {
           return done(null, false, {
-            success : false,
-            message : "unregistered email",
+            success: false,
+            message: "unregistered email",
           });
         }
         // Password is null i.e registered using google or github
         if (!user.password) {
           return done(null, false, {
-            message : "Invalid login mode",
-            success : false,
+            message: "Invalid login mode",
+            success: false,
           });
         }
         const sPassword = sanitize(password);
@@ -116,22 +127,26 @@ passport.use(new LocalStrategy(
               return done(null, user._id);
             }
             return done(null, false, {
-              message : "Please verify your email first",
+              message: "Please verify your email first",
             });
           }
           // incorrect password
           return done(null, false, {
-            message : "Invalid Credentials",
-            success : false,
+            message: "Invalid Credentials",
+            success: false,
           });
         });
       });
-    }));
+    }
+  )
+);
 //--------------------------------------------------END  OF LOCAL
 // STRATEGY----------------------------------------//
 //------------------------------------------------SERIALIZERS AND
 // DESERIALIZERS-----------------------------------//
-passport.serializeUser((id, done) => { done(null, id); });
+passport.serializeUser((id, done) => {
+  done(null, id);
+});
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then((user) => {
