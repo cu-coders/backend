4e8441b6e67d34d0595254f01c2ab0a9d650ad39
Restyled.io <commits@restyled.io>
2022-02-25 19:50:46 +0000

    Restyled by clang-format

--- models/project.js
+++ models/project.js
@@ -1,18 +1,18 @@
 "use strict"
 const mongoose = require("mongoose");
 const projectSchema = mongoose.Schema({
-    name: {
-        type: String,
-        require: true,
-    },
-    email: {
-        type: String,
-        require: true,
-    },
-    description: {
-        type: String,
-        require: true,
-    }
+  name : {
+    type : String,
+    require : true,
+  },
+  email : {
+    type : String,
+    require : true,
+  },
+  description : {
+    type : String,
+    require : true,
+  }
 });
 const Project = mongoose.model("project", projectSchema);
 module.exports = Project;
--- routes/projectForm_routes.js
+++ routes/projectForm_routes.js
@@ -1,17 +1,17 @@
 "use strict";
 const express = require("express");
-const { validationResult } = require("express-validator");
+const {validationResult} = require("express-validator");
 const rules = require("../middlewares/validation-rules");
 const projectDbApis = require("../controllers/projectDBApis");
 const router = express.Router();
 // const csrf = require("csurf");
 // const csrfProtection = csrf({ cookie: true });
 router.post("/", rules.projectForm, async (req, res) => {
-    const validationErr = validationResult(req);
-    if (validationErr.isEmpty()) {
-        await projectDbApis.insertProject(req, res);
-    } else {
-        res.jsonp({ success: false, err: validationErr.array() });
-    }
+  const validationErr = validationResult(req);
+  if (validationErr.isEmpty()) {
+    await projectDbApis.insertProject(req, res);
+  } else {
+    res.jsonp({success : false, err : validationErr.array()});
+  }
 });
 module.exports = router;
