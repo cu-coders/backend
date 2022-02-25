a3e38cccd1d8d32684cd800d83808e107569ae4f
Restyled.io <commits@restyled.io>
2022-02-25 20:29:40 +0000

    Restyled by clang-format

--- controllers/projectDBApis.js
+++ controllers/projectDBApis.js
@@ -3,22 +3,21 @@ const sanitizer = require("mongo-sanitize");
 const Project = require("../models/message");
 const mailer = require("./mailer");
 //--------------------------------END OF
-//IMPORTS---------------------------------------//
+// IMPORTS---------------------------------------//
 exports.insertProject = async (req, res) => {
-    const name = sanitizer(req.body.name);
-    const email = sanitizer(req.body.email);
-    const details = sanitizer(req.body.description);
-    try {
-        const project = new Project({
-            name,
-            email,
-            details,
-        });
-        await mailer.sendAck(project.email, project.name, project.description);
-        await project.save();
-        res.status(200).json({ success: true });
-    } catch (err) {
-        res.status(500).json({ success: false });
-    }
+  const name = sanitizer(req.body.name);
+  const email = sanitizer(req.body.email);
+  const details = sanitizer(req.body.description);
+  try {
+    const project = new Project({
+      name,
+      email,
+      details,
+    });
+    await mailer.sendAck(project.email, project.name, project.description);
+    await project.save();
+    res.status(200).json({success : true});
+  } catch (err) {
+    res.status(500).json({success : false});
+  }
 };
-
