42a6b5205048905dd5e5afd16597d966e1bf6bd8
Restyled.io <commits@restyled.io>
2022-02-26 05:33:21 +0000

    Restyled by clang-format

--- controllers/projectDBApis.js
+++ controllers/projectDBApis.js
@@ -1,23 +1,23 @@
 "use strict";
-const  sanitize  = require("mongo-sanitize");
+const sanitize = require("mongo-sanitize");
 const Project = require('../models/project');
 const mailer = require("./mailer");
 //--------------------------------END OF
-//IMPORTS---------------------------------------//
+// IMPORTS---------------------------------------//
 exports.insertProject = async (req, res) => {
-    const name = sanitize(req.body.name);
-    const email = sanitize(req.body.email);
-    const sDetails = sanitize(req.body.details);
-    try {
-        const project = new Project({
-        email,
-        name,
-        details: sDetails,
-        });
-        //await mailer.sendAck(message.email, message.name);
-        await project.save();
-        res.status(200).json({ success: true });
-    } catch (err) {
-        res.status(500).json({ success: false });
-    }
+  const name = sanitize(req.body.name);
+  const email = sanitize(req.body.email);
+  const sDetails = sanitize(req.body.details);
+  try {
+    const project = new Project({
+      email,
+      name,
+      details : sDetails,
+    });
+    // await mailer.sendAck(message.email, message.name);
+    await project.save();
+    res.status(200).json({success : true});
+  } catch (err) {
+    res.status(500).json({success : false});
+  }
 };
--- routes/projectForm_routes.js
+++ routes/projectForm_routes.js
@@ -1,6 +1,6 @@
 "use strict";
 const express = require("express");
-const { validationResult } = require("express-validator");
+const {validationResult} = require("express-validator");
 const rules = require("../middlewares/validation-rules");
 const projectDbApis = require("../controllers/projectDBApis");
 const router = express.Router();
@@ -8,11 +8,11 @@ const router = express.Router();
 // const csrfProtection = csrf({ cookie: true });
 
 router.post("/", rules.projectForms, async (req, res) => {
-    const validationErr = validationResult(req);
-    if (validationErr.isEmpty()) {
-        await projectDbApis.insertProject(req, res);
-    } else {
-        res.jsonp({ success: false, err: validationErr.array() });
-    }
+  const validationErr = validationResult(req);
+  if (validationErr.isEmpty()) {
+    await projectDbApis.insertProject(req, res);
+  } else {
+    res.jsonp({success : false, err : validationErr.array()});
+  }
 });
 module.exports = router;
