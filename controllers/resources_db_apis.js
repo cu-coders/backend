49d8c10ce9de7a5cc60417e8e00d827f49745fc3
Restyled.io <commits@restyled.io>
2022-02-27 07:18:09 +0000

    Restyled by clang-format

--- controllers/resources_db_apis.js
+++ controllers/resources_db_apis.js
@@ -5,33 +5,31 @@ const sanitize = require("mongo-sanitize");
 const mailer = require("./mailer");
 
 exports.insert_resource = async (req, res, imageURL, public_id) => {
-  const uploadResult = await cloudinaryConfig.v2.uploader.upload(
-    req.file.path,
-    {
-      folder: `resources/${req.body.category}`,
-      use_filename: true,
-      resource_type: "auto",
-    }
-  );
-  const { secure_url, public_id_1 } = uploadResult;
+  const uploadResult =
+      await cloudinaryConfig.v2.uploader.upload(req.file.path, {
+        folder : `resources/${req.body.category}`,
+        use_filename : true,
+        resource_type : "auto",
+      });
+  const {secure_url, public_id_1} = uploadResult;
   const title = sanitize(req.body.title);
   const subtitle = sanitize(req.body.subtitle);
   const description = sanitize(req.body.description);
   const category = sanitize(req.body.category);
   const newResources = new resources({
-    imageSrc: imageURL,
-    imageId: public_id,
+    imageSrc : imageURL,
+    imageId : public_id,
     title,
     subtitle,
     description,
     category,
-    resources: secure_url,
-    resourcesId: public_id_1,
+    resources : secure_url,
+    resourcesId : public_id_1,
   });
   try {
-    //await mailer.applicationAck(email, role, name);
+    // await mailer.applicationAck(email, role, name);
     await newResources.save();
-    res.json({ success: true, message: "Resource Added Successfully!" });
+    res.json({success : true, message : "Resource Added Successfully!"});
   } catch (error) {
     await cloudinaryConfig.uploader.destroy(newResources.resourcesId);
     throw error;
@@ -39,6 +37,6 @@ exports.insert_resource = async (req, res, imageURL, public_id) => {
 };
 
 exports.getResources = async (res) => {
-    const resourcesData = await resources.find();
-    res.json({ success: true, data: resourcesData });
+  const resourcesData = await resources.find();
+  res.json({success : true, data : resourcesData});
 };
